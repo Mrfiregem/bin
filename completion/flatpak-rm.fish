fa85bcbb5a0ee42502751183df476bd9bfff0f44
Firegem <mrfiregem@protonmail.ch>
2023-01-05 08:03:23 -0500

    flatpak-rm: complete names

--- completion/flatpak-rm.fish
+++ completion/flatpak-rm.fish
@@ -1,5 +1,5 @@
 set -l name flatpak-rm
-complete -c $name -f
+complete -c $name -f -a '(flatpak list --columns=name)' -d query
 complete -c $name -x -s h -l help -d 'show help'
 complete -c $name -x -s a -l app -d 'filter applications'
 complete -c $name -x -s r -l runtime -d 'filter runtimes'
